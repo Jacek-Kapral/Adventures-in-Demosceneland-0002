6b5c730a1f40386ee406ff7b30b15b90d6965143
Jacek-Kapral <jacek.kapral@proton.me>
2026-02-25 23:36:49 +0100

    Add background color and font change

--- main.lua
+++ main.lua
@@ -4,7 +4,8 @@ end
 function love.update(dt)
 end
 function love.draw()
-    love.graphics.setColor(0, 0, 0)
+    love.graphics.clear(0, 0, 0)
+    love.graphics.setColor(0.6, 0.6, 0.6)
     local w, h = love.graphics.getDimensions()
     local fh = love.graphics.getFont():getHeight()
     love.graphics.printf ("Hello World!", 0, h/2 - fh/2, w, "center")
