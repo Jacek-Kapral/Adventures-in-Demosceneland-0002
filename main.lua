63b598a1a4672f2c1b090edd387c4b689c56b944
Jacek-Kapral <jacek.kapral@proton.me>
2026-02-25 23:55:02 +0100

    Change simple Hello World display to sine wave

--- main.lua
+++ main.lua
@@ -1,14 +1,28 @@
 function love.load()
     love.window.setMode(640, 480)
+    waveTime = 0
 end
 function love.update(dt)
+    waveTime = waveTime + dt
 end
 function love.draw()
+    amplitude = 12
+    phase = 0.4
     love.graphics.clear(0, 0, 0)
     love.graphics.setColor(0.6, 0.6, 0.6)
+    local text = "Hello world!"
+    local font = love.graphics.getFont()
+    local fh = font:getHeight()
     local w, h = love.graphics.getDimensions()
-    local fh = love.graphics.getFont():getHeight()
-    love.graphics.printf ("Hello World!", 0, h/2 - fh/2, w, "center")
+    local totalWidth = font:getWidth(text)
+    local startX = (w - totalWidth) / 2
+    local baseY = h/2 0 fh/2
+    for i = 1, #text do
+        local c = text:sub(i, i)
+        local startX + font:getWidth(text:sub(1, i-1))
+        local offset = amplitude * math.sin(waveTime + (i -1) * phase)
+        love.graphics.printf (c, x, baseY + offset)
+    end
 end
 
 
